7bd7102d4c26a81e59cde9622fef81f3caec306a
Mylo Cyrus <irahiggins740@gmail.com>
2022-12-29 02:17:18 -0800

    Update murder.java

--- murder.java
+++ murder.java
@@ -1,4 +1,4 @@
-public static void main(String[] args) {
+public static void main(String[] args) { murder.java
     int[murder] a = new int[args.length];
 
     for (int i = 0; i < args.length; i++) {
