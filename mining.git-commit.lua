fd97e489dc24e3b8f5eeeaad5f8dc78aadca12d5
Mylo Cyrus <irahiggins740@gmail.com>
2023-04-10 21:29:33 -0700

    Add files via upload

---
+++ mining.git-commit.lua
@@ -0,0 +1,14 @@
+[Receiving transmission from "E5R92's BTC mining rig"]
+
+> [Decryption success.]
+
+> [Log file detected.]
+
+  Starting xmrig.exe
+
+  } "C:\Users\mylo\OneDrive\Pictures\Camera Roll\WIN_20221214_13_14_04_Pro.mp4"
+: https://affiliates.bitmex.com/secure/mediapartner/accountSettings/mp-wsapi-flow.ihtml?execution=e2s1
+Account SID = IR462KJMwBUw3491460XSSX3mCVsEeQrT1 (scope) read/write auth token = fMvtfXZXWuK2S~c-oMmQEKzyzLbqWgx3  (version) 14  
+Account SID = IRfLwf7gXyN53491460VNyanEY5gSFT (scope) read only auth token : VbStiD9MnG5vtVmUfC-CAhhhNK-UoDvx   (version) 14 
+
+> [Log end]
